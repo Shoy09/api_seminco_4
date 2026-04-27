81effcb131fa29c2aefd150fb951d1606525e5b5
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2026-04-26 22:31:56 -0500

    check_list_telemando-eliminacion

--- src/models/OperacionCarguio.js
+++ src/models/OperacionCarguio.js
@@ -17,7 +17,6 @@ const OperacionCarguio = sequelize.define('OperacionCarguio', {
     condiciones_equipo: DataTypes.TEXT,
     programa_trabajo: DataTypes.TEXT,
     check_list: DataTypes.TEXT,
-    check_list_telemando: DataTypes.TEXT,
     control_llantas: DataTypes.TEXT,
     estado: { type: DataTypes.STRING, defaultValue: 'activo' },
     envio: { type: DataTypes.INTEGER, defaultValue: 0 },
